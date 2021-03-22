6e21dd12c3216e7e63f0f2d2d17683ea8f6a55ee
aznamle <namle1998@hotmail.com>
2021-03-22 11:48:17 -0700

    fixed twoblockimageright's header

--- components/slices/TwoBlockImageRight.js
+++ components/slices/TwoBlockImageRight.js
@@ -6,7 +6,7 @@ const TwoBlockImageRight = ({ slice }) => (
         <div className="bg-white flex justify-center items-center lg:w-1/2 shadow-lg">
             <div className="p-20 text-left">
                 <h3 className="text-lg lg:text-3xl font-semibold text-black">
-                    {RichText.render(slice.primary.blocktitle[0].text)}
+                    {slice.primary.blocktitle[0].text}
                 </h3>
                 {slice.primary.blockdescription.map((text) => (
                     <p className="mt-4 whitespace-pre-line text-lg md:text-base lg:text-xl leading-relaxed text-gray-600">
