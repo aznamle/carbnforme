50d1965c275553e02584751760c860b4e25fd70f
aznamle <namle1998@hotmail.com>
2021-03-24 17:43:16 -0700

    added [uid] page for dynamic pages, added queries util for uid pages

---
+++ pages/[uid].js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Client } from '../prismic-configuration'
+import { SliceZone } from '../components'
+import { queryRepeatableDocuments } from '../utils/queries'
+
+
+export default function Page({ doc }) {
+    console.log(doc)
+    if (doc && doc.data) {
+      return (
+          <div>
+            <SliceZone sliceZone={doc.data.body} />
+          </div>
+          
+      )
+    }
+}
+
+export async function getStaticProps({ params, preview = null, previewData = {} }) {
+  
+    const { ref } = previewData
+    const client = Client();
+    const doc = await client.getByUID('page', params.uid, ref ? { ref } : null) || {}
+  
+    return {
+      props: {
+        preview,
+        doc,
+      },
+    }
+  }
+
+  export async function getStaticPaths() {
+    const documents = await queryRepeatableDocuments((doc) => doc.type === 'page')
+    return {
+      paths: documents.map(doc => `/${doc.uid}`),
+      fallback: true,
+    }
+  }
+  
