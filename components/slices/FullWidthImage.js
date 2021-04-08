944b7154695f45949c44374e65a276cec68ebddf
aznamle <namle1998@hotmail.com>
2021-04-08 16:16:30 -0700

    adjustments for mobile

--- components/slices/FullWidthImage.js
+++ components/slices/FullWidthImage.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
 const FullWidthImage = ({ slice }) => (
-  <div className='flex-wrap content-center justify-center'>
     <div className="flex w-100 h-screen items-center bg-center md:bg-fixed bg-no-repeat lg:bg-cover"
         style={{
           backgroundImage: `url(${slice.primary.banner.url})`
         }}>
     </div>
-  </div>
 )
 
 export default FullWidthImage
