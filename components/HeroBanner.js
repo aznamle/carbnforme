23147f022c3c4e9975d489f72425d38e8df02994
aznamle <namle1998@hotmail.com>
2021-04-08 11:23:12 -0700

    fixed herobanner component on mobile

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function HeroBanner({ banner }) {
     return (
-        <div className="flex w-full h-screen items-center lg:h-screen bg-top bg-fixed bg-no-repeat bg-cover"
+        <div className="flex w-full h-screen items-center bg-top bg-fixed bg-no-repeat bg-cover"
             style={{
-              backgroundImage: `url(${banner.url})`
+              backgroundImage: `url(${banner.url})`,
+              Height: "100vh"
             }}>
         </div>
     )
