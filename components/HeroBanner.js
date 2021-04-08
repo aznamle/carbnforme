edce9e45babb0641cd4631820dc35822d2f98796
aznamle <namle1998@hotmail.com>
2021-04-08 11:29:05 -0700

    more adjustments to herobanner component

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 export default function HeroBanner({ banner }) {
     return (
-        <div className="flex w-full h-screen items-center bg-top bg-fixed bg-no-repeat bg-cover"
+        <div className="flex w-100 h-100 h-screen items-center bg-top bg-fixed bg-no-repeat bg-cover"
             style={{
               backgroundImage: `url(${banner.url})`,
-              Height: "100vh"
             }}>
         </div>
     )
