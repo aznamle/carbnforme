778aac7f6dac6066b423db667e24b1be0798b299
aznamle <namle1998@hotmail.com>
2021-04-12 17:06:02 -0700

    added content to videobanner slice

--- components/slices/VideoBanner.js
+++ components/slices/VideoBanner.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const VideoBanner = () => {
+const VideoBanner = ({ slice }) => {
     return (
-        <div>
-            
+        <div className="flex flex-wrap">
+            <video src={RichText.asText(slice.primary.videourl)} type='video/mp4' autoPlay muted loop playsInline alt="video" 
+                className="object-cover object-center h-full w-full md:h-auto lg:w-1/2" 
+            />
         </div>
     )
 }
