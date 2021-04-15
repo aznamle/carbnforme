52d528e44dc561dc6460976d1b464a990268a9d4
aznamle <namle1998@hotmail.com>
2021-04-15 15:56:26 -0700

    more styling adjustments to feature slice

--- components/slices/Feature.js
+++ components/slices/Feature.js
@@ -3,14 +3,14 @@ import { RichText } from 'prismic-reactjs'
 
 const Feature = ({slice}) => {
     return (
-        <div className='flex flex-wrap py-24 px-64 h-full justify-center content-center items-center bg-black'>
+        <div className='flex flex-wrap lg:py-24 md:px-16 lg:px-32 h-full justify-center content-center items-center bg-black'>
             <div className="lg:w-2/6 text-center">
-                <h1 className="text-3xl lg:text-6xl font-semibold text-white">
+                <h1 className="text-3xl lg:text-5xl font-semibold text-white">
                     {RichText.asText(slice.primary.featuretitle)}
                 </h1>
             </div>
 
-            <div className="lg:w-4/6 px-24">
+            <div className="text-center lg:text-left lg:w-4/6 lg:px-24">
             <p className="mt-4 whitespace-pre-line text-lg md:text-2xl font-semibold lg:text-2xl leading-relaxed text-white">
                 {RichText.asText(slice.primary.featuredescription)}
             </p>
