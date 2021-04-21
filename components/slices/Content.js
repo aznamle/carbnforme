bace0b59693a6500c4e9482419a31d37ce60006a
aznamle <namle1998@hotmail.com>
2021-04-21 15:14:14 -0700

    added styling to content slice

--- components/slices/Content.js
+++ components/slices/Content.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { RichText } from 'prismic-reactjs'
 
-const Content = () => {
+const Content = ({ slice }) => {
     return (
-        <div>
-            
+        <div className="flex h-full">
+            <h1 className="font-semibold">{RichText.asText(slice.primary.sectiontitle)}</h1>
+            <div>
+                {slice.items.map((content) => (
+
+
+                <div>
+                    <h1>{RichText.asText(content.contenttitle)}</h1>
+                    <p>{RichText.asText(content.contentdescription)}</p>
+                {content.leftalign === true ? <img src={content.contentimage.url} alt="left aligned"/>
+                    : <img src={content.contentimage.url} alt="right aligned" />
+                }
+                </div>
+
+
+
+                ))}
+            </div>
         </div>
     )
 }
