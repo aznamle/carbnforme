d475e933e3cd384e74c2a578e8099d1ce9832f3f
aznamle <namle1998@hotmail.com>
2021-04-22 10:44:21 -0700

    fixed mobile display for content slice

--- components/slices/Content.js
+++ components/slices/Content.js
@@ -3,43 +3,48 @@ import { RichText } from 'prismic-reactjs'
 
 const Content = ({ slice }) => {
     return (
-        <div className="h-full w-100">
+        <div className="max-w-7xl px-6 py-16 mx-auto">
             <div className="items-center justify-items-center w-full">
                 <h1 className="font-semibold">{RichText.asText(slice.primary.sectiontitle)}</h1>
             </div>
-            <div>
-                {slice.items.map((content) => (
 
+                <div className="">
+                    {slice.items.map((content) => (
+                    <div className="flex flex-wrap w-100">
+                        <>
+                            {content.leftalign === false ? 
+                                <div className="items-center justify-center md:w-1/2">
+                                    <img className="object-cover object-center w-full md:h-auto" src={content.contentimage.url} alt="right aligned"/>
+                                </div>
+                                : undefined
+                            }
+                        </>
+                        
+                        <div className="flex justify-center items-center md:w-1/2">
+                            <div className="p-10 md:p-20 text-left">
 
-                <div className="flex">
-
-                    <>
-                        {content.leftalign === false ? 
-                            <div className="flex flex-wrap justify-center items-center w-full lg:w-1/2">
-                                <img className="object-cover object-center h-full w-full md:h-auto" src={content.contentimage.url} alt="right aligned"/>
+                                <h1 className="text-3xl lg:text-6xl font-semibold ">
+                                    {RichText.asText(content.contenttitle)}
+                                </h1>
+                                <p className="mt-4 whitespace-pre-line text-lg md:text-base lg:text-xl leading-relaxed text-gray-600">
+                                    {RichText.asText(content.contentdescription)}
+                                </p>
                             </div>
-                            : undefined
-                        }
-                    </>
-                    
-                    <div className="justify-center items-center w-full lg:w-1/2">
-                        <h1 className="text-3xl">{RichText.asText(content.contenttitle)}</h1>
-                        <p>{RichText.asText(content.contentdescription)}</p>
+                        </div>
+
+                        <>
+                            {content.leftalign === true ? 
+                                <div className="justify-center items-center w-full md:w-1/2">
+                                    <img className="object-cover object-center h-full w-full" src={content.contentimage.url} alt="left aligned"/>
+                                </div>
+                                : undefined
+                            }
+                        </>
                     </div>
 
-                    <>
-                        {content.leftalign === true ? 
-                            <div className="flex flex-wrap justify-center items-center w-full lg:w-1/2">
-                                <img className="object-cover object-center h-full w-full" src={content.contentimage.url} alt="left aligned"/>
-                            </div>
-                            : undefined
-                        }
-                    </>
-                </div>
 
 
-
-                ))}
+                    ))}
             </div>
         </div>
     )
