e29737d250ee5b45bed56653f07cdb1c61bf7ff9
aznamle <namle1998@hotmail.com>
2021-04-22 10:00:30 -0700

    fixed herobanner component not displaying

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -2,19 +2,17 @@ import React from 'react'
 
 export default function HeroBanner({ banner }) {
 
-    const urlEmpty = banner
+    const urlEmpty = banner.url
 
     return (
         <div>
-        { urlEmpty == "" ? <div className="flex w-100 h-screen items-center bg-center md:bg-fixed bg-no-repeat bg-cover"
+        { urlEmpty !== "" ? <div className="flex w-100 h-screen items-center bg-center md:bg-fixed bg-no-repeat bg-cover"
                 style={{
                   backgroundImage: `url(${banner.url})`
                 }}>
             </div>
             :
-            <div>
-                
-            </div>
+            undefined
         }
         </div>
     )
