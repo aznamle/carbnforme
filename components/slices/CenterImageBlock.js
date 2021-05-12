e1f2fba76d6246415542e5f0b37674f9005e3bcf
aznamle <namle1998@hotmail.com>
2021-05-12 11:08:01 -0700

    mobile padding for title

--- components/slices/CenterImageBlock.js
+++ components/slices/CenterImageBlock.js
@@ -3,7 +3,7 @@ import { RichText } from 'prismic-reactjs'
 
 const CenterImageBlock = ({ slice }) => {
     return (
-        <div className='md:max-w-screen-2xl mx-auto py-12'>
+        <div className='md:max-w-screen-2xl mx-auto pb-12 lg:pb-0 lg:py-12'>
             <img src={slice.primary.center_image.url} className='' />
             <div className='lg:flex w-full gap-x-24 items-center justify-center py-12 text-center lg:text-left px-12 lg:px-0'>
                 <h1 className='text-4xl lg:1/3 py-4'>{RichText.asText(slice.primary.side_title)}</h1>
